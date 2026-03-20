25bf5aad40fb5dc80996323b08a19bdec2941c76
BtheBFr <rtfmqwertu29v@gmail.com>
2026-03-20 13:13:18 +0500

    Update games.js

--- games.js
+++ games.js
@@ -1,39 +1,20 @@
-// База данных игр
 const gamesDatabase = [
     {
         id: 'snake',
-        name: 'Snake', // Отображаемое название
-        ru: 'Змейка, змея', // Для поиска по-русски
-        en: 'Snake', // Для поиска по-английски
+        name: 'Snake',
+        ru: 'Змейка, змея',
+        en: 'Snake',
         icon: 'icons/snake.png',
         path: 'games/snake/',
-        modes: ['1P'] // Только одиночная
+        modes: ['1P']
     },
     {
         id: 'tetris',
         name: 'Tetris',
-        ru: 'Тетрис, тетрис',
+        ru: 'Тетрис',
         en: 'Tetris',
         icon: 'icons/tetris.png',
         path: 'games/tetris/',
         modes: ['1P', 'bot']
-    },
-    {
-        id: 'pong',
-        name: 'Pong',
-        ru: 'Понг, теннис',
-        en: 'Pong',
-        icon: 'icons/pong.png',
-        path: 'games/pong/',
-        modes: ['multi(2)', 'bot']
-    },
-    {
-        id: 'chess',
-        name: 'Chess',
-        ru: 'Шахматы, шахматы',
-        en: 'Chess',
-        icon: 'icons/chess.png',
-        path: 'games/chess/',
-        modes: ['multi(2)', 'bot']
     }
 ];
